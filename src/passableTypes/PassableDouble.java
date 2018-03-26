562dd3ee5d095a4f15f771b6037535de5a440e7f
Andrew LeMaitre <AndrewLeMaitre@gmail.com>
2018-03-26 10:38:13 -0500

    Created a PassableDouble object.

---
+++ src/passableTypes/PassableDouble.java
@@ -0,0 +1,25 @@
+package passableTypes;
+
+import java.io.Serializable;
+
+public class PassableDouble implements Serializable {
+	
+    /** Generated serial id */
+    private static final long serialVersionUID = -3938763484007618097L;
+    private double value;
+	
+	public PassableDouble() {}
+	
+	public PassableDouble( double value )
+	{
+		this.value = value;
+	}
+	
+	public void setValue( double i ) {
+		value = i;
+	}
+	
+	public double getValue() {
+		return value;
+	}
+}
