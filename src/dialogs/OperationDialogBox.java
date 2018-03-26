1f8401b97bc0449681f2a9a3f475fca82e762ca5
Andrew LeMaitre <AndrewLeMaitre@gmail.com>
2018-03-26 10:36:45 -0500

    Added new 1 dimensional spinner control for the operation dialog box.

--- src/dialogs/OperationDialogBox.java
+++ src/dialogs/OperationDialogBox.java
@@ -83,6 +83,27 @@ public class OperationDialogBox {
 		
 	}
 	
+   public JSpinner[] add1DDimension( String label, PassableDouble passableDouble, SpinnerNumberModel xModel )
+    {
+        addMigRow();
+        addSettingLabel( label );
+
+        //Create a new JSpinner model for the x (or width) value of the dimension
+        JSpinner dimXSpinner = new JSpinner( xModel );
+        dimXSpinner.setPreferredSize( new Dimension( 60, 20 ) );
+        dimXSpinner.setMinimumSize( new Dimension( 60, 20 ) );
+        
+        //Add change listeners to the spinners
+        dimXSpinner.addChangeListener( evt -> passableDouble.setValue( (double)dimXSpinner.getValue() ) ); 
+
+        //Add the spinners and the x label between them to the dialog.
+        addSettingControl( dimXSpinner, "", "flowx" );
+        
+        elementCount++;
+        
+        return new JSpinner[] { dimXSpinner };
+    }
+	
 	public JSpinner[] add2DDimension( String label, Dimension2D dimension, SpinnerNumberModel xModel, SpinnerNumberModel yModel, boolean isSquare )
 	{
 		addMigRow();
